e2902441c0d6810bc2824a0f3e044479ce1c2e46
lapressle <112499468+lapressle@users.noreply.github.com>
2023-12-26 13:54:41 -0500

    Add each_with_index function that passes spec

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,14 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    arr = self
+    counter = 0
+    for i in arr
+      arr =  yield(i, counter)
+      counter += 1
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
